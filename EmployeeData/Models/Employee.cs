69249f80accfbe7f9ad52fae19ec70ba2c2dacd7
Cris0221 <cristobal.fragata@pointwest.com.ph>
2022-05-04 18:21:50 +0800

    model validations

--- EmployeeData/Models/Employee.cs
+++ EmployeeData/Models/Employee.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 #nullable disable
 
@@ -19,13 +20,18 @@ namespace EmployeeData.Models
         [Column("EmployeeID")]
         public int EmployeeId { get; set; }
         [Required]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "First name length must not exceed 50 characters")]
+        [MinLength(2, ErrorMessage = "First Name should contain atleast 2 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First name must not contain special characters")]
         public string FirstName { get; set; }
         [Required]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "Last name length must not exceed 50 characters")]
+        [MinLength(2, ErrorMessage = "Last Name should contain atleast 2 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last name must not contain special characters")]
         public string LastName { get; set; }
         [Required]
-        [StringLength(12)]
+        [StringLength(12, ErrorMessage = "Phone must not exceed 12 characters")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "Phone must only contain numbers")]
         public string HomePhone { get; set; }
         public bool Active { get; set; }
 
